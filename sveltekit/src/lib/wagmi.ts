9ee63ddeb379cfab09a1f24124abd747a6799cd9
zapaz.eth <alain@kredeum.com>
2024-08-21 18:42:46 +0200

    add Coinbase Smart wallet

--- sveltekit/src/lib/wagmi.ts
+++ sveltekit/src/lib/wagmi.ts
@@ -1,45 +1,44 @@
 import { createBurnerConnector } from "./burner-wallet/createBurnerConnector";
 import { getAlchemyHttpUrl } from "./utils/scaffold-eth/networks";
-import { createWagmiConfig, http } from "@byteatatime/wagmi-svelte";
-import { anvil, mainnet, type Chain } from "@byteatatime/wagmi-svelte/chains";
-import { coinbaseWallet, injected, walletConnect } from "@byteatatime/wagmi-svelte/connectors";
-import { createClient } from "viem";
-import * as chains from "viem/chains";
+import { createWagmiConfig } from "@byteatatime/wagmi-svelte";
+import { coinbaseWallet, injected, walletConnect } from '@wagmi/connectors'
+import { createClient, http } from "viem";
+import { anvil, mainnet, type Chain } from "viem/chains";
 import scaffoldConfig from "$lib/scaffold.config";
 
 const { onlyLocalBurnerWallet, walletConnectProjectId, targetNetworks } = scaffoldConfig;
 
 export const enabledChains = targetNetworks.find((network: Chain) => network.id === 1)
-	? targetNetworks
-	: ([...targetNetworks, mainnet] as const);
+  ? targetNetworks
+  : ([...targetNetworks, mainnet] as const);
 
 const connectors = [
-	injected(),
-	walletConnect({
-		projectId: walletConnectProjectId,
-		showQrModal: false
-	}),
-	coinbaseWallet({
-		appName: "scaffold-eth-2"
-	}),
-	...(!targetNetworks.some((network) => network.id !== (chains.anvil as chains.Chain).id) ||
-	!onlyLocalBurnerWallet
-		? [createBurnerConnector()]
-		: [])
+  injected(),
+  walletConnect({
+    projectId: walletConnectProjectId,
+    showQrModal: false
+  }),
+  coinbaseWallet({
+    appName: "scaffold-eth-2", preference: 'all',
+  }),
+  ...(!targetNetworks.some((network) => network.id !== anvil.id) ||
+    !onlyLocalBurnerWallet
+    ? [createBurnerConnector()]
+    : [])
 ];
 
 export const wagmiConfig = createWagmiConfig({
-	chains: enabledChains,
-	connectors,
-	client({ chain }) {
-		return createClient({
-			chain,
-			transport: http(getAlchemyHttpUrl(chain.id)),
-			...(chain.id === (anvil as Chain).id
-				? {
-						pollingInterval: scaffoldConfig.pollingInterval
-					}
-				: {})
-		});
-	}
+  chains: enabledChains,
+  connectors,
+  client({ chain }) {
+    return createClient({
+      chain,
+      transport: http(getAlchemyHttpUrl(chain.id)),
+      ...(chain.id === (anvil as Chain).id
+        ? {
+          pollingInterval: scaffoldConfig.pollingInterval
+        }
+        : {})
+    });
+  }
 });
