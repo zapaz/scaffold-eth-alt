4a6a62ffe6a8588e3597841a1872cef6b984b752
zapaz.eth <alain@kredeum.com>
2024-11-12 15:05:42 +0100

    fix <Connect button

--- svelte5/src/lib/scaffold.config.ts
+++ svelte5/src/lib/scaffold.config.ts
@@ -11,13 +11,13 @@ export type ScaffoldConfig = {
 };
 
 const { PUBLIC_ALCHEMY_API_KEY, PUBLIC_WALLET_CONNECT_PROJECT_ID } = env as {
-  PUBLIC_ALCHEMY_API_KEY?: string;
-  PUBLIC_WALLET_CONNECT_PROJECT_ID?: string;
+  PUBLIC_ALCHEMY_API_KEY: string;
+  PUBLIC_WALLET_CONNECT_PROJECT_ID: string;
 };
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.anvil, chains.sepolia, chains.optimism],
+  targetNetworks: [chains.mainnet, chains.base, chains.optimism, chains.baseSepolia, chains.optimismSepolia, chains.anvil],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
@@ -27,13 +27,13 @@ const scaffoldConfig = {
   // You can get your own at https://dashboard.alchemyapi.io
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  alchemyApiKey: PUBLIC_ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
+  alchemyApiKey: PUBLIC_ALCHEMY_API_KEY,
 
   // This is ours WalletConnect's default project ID.
   // You can get your own at https://cloud.walletconnect.com
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
-  walletConnectProjectId: PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
+  walletConnectProjectId: PUBLIC_WALLET_CONNECT_PROJECT_ID,
 
   // Only show the Burner Wallet when running on local network
   onlyLocalBurnerWallet: true,
--- svelte5/src/routes/tests/connect/+page.svelte
+++ svelte5/src/routes/tests/connect/+page.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
   import { Connect } from "$lib/wagmi/components";
+  import { createAccount } from "$lib/wagmi/runes";
 
   let chainId = $state(undefined);
   let address = $state(undefined);
-  let name = $state(undefined);
 
-  $inspect("address:", address);
+  let account = createAccount();
+
+  $inspect("<Connect account:", account);
+  $inspect("<Connect address:", address);
 </script>
 
 <div class="p-8">
-  <Connect bind:chainId bind:address bind:name />
+  <Connect bind:chainId bind:address />
 </div>
 
-<div class="px-8">
-  name: {name}
-</div>
 <div class="px-8">
   address: {address}
 </div>
 <div class="px-8">
   chainId: {chainId}
+</div>
+
+<div class="px-8">
+  account: {account?.account.address}
 </div>
